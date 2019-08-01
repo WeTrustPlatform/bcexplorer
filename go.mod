c6d37ec20a6628e60465d85453fccbef0b48fc6e
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-08-01 16:06:16 +0700

    block handler

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/gogo/protobuf v1.2.1
+	github.com/google/gofuzz v1.0.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway v1.9.5 // indirect
 	github.com/prysmaticlabs/go-bitfield v0.0.0-20190720014013-035bd0bea8f0 // indirect
 	github.com/prysmaticlabs/prysm v0.0.0-20190731224611-08ac1c3c353f
--- main.go
+++ main.go
@@ -14,22 +14,22 @@ import (
 	"google.golang.org/grpc"
 )
 
-func displayBlock(
-	ctx context.Context,
-	w http.ResponseWriter,
-	client pbeth.BeaconChainClient,
-	block *pbeth.BeaconBlock) *pbeth.BeaconBlock {
-
-	hash := hex.EncodeToString(block.StateRoot)
-	nat := len(block.GetBody().Attestations)
-	w.Write([]byte("<p><a href=\"/bl/" + hash + "\">" + hash + "</a> - " + fmt.Sprintf("%d", nat) + " attestations</p>"))
-
-	resp, _ := client.ListBlocks(ctx, &pbeth.ListBlocksRequest{
-		QueryFilter: &pbeth.ListBlocksRequest_Root{Root: block.ParentRoot},
+func getBlock(ctx context.Context, client pbeth.BeaconChainClient, root []byte) *pbeth.BeaconBlock {
+	resp, err := client.ListBlocks(ctx, &pbeth.ListBlocksRequest{
+		QueryFilter: &pbeth.ListBlocksRequest_Root{Root: root},
 		PageToken:   strconv.Itoa(0),
 	}, &grpc.EmptyCallOption{})
+	if err != nil {
+		panic(err)
+	}
 
 	return resp.Blocks[0]
+}
+
+func displayBlock(w http.ResponseWriter, block *pbeth.BeaconBlock) {
+	root := hex.EncodeToString(block.StateRoot)
+	nat := len(block.GetBody().Attestations)
+	w.Write([]byte("<p><a href=\"/block?root=" + root + "\">" + root + "</a> - " + fmt.Sprintf("%d", nat) + " attestations</p>"))
 }
 
 func main() {
@@ -43,9 +43,6 @@ func main() {
 	beaconClient := pb.NewBeaconServiceClient(conn)
 	ethClient := pbeth.NewBeaconChainClient(conn)
 
-	http.HandleFunc("/bl/", func(w http.ResponseWriter, r *http.Request) {
-	})
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		block, err := beaconClient.CanonicalHead(ctx, &ptypes.Empty{})
 		if err != nil {
@@ -56,8 +53,24 @@ func main() {
 		w.Write([]byte("<h1>Latest blocks</h1>"))
 
 		for i := 0; i < 20 && block != nil; i++ {
-			block = displayBlock(ctx, w, ethClient, block)
+			displayBlock(w, block)
+			fmt.Println(block.StateRoot, hex.EncodeToString(block.StateRoot))
+			block = getBlock(ctx, ethClient, block.ParentRoot)
 		}
+	})
+
+	http.HandleFunc("/block", func(w http.ResponseWriter, r *http.Request) {
+		rootStrings, _ := r.URL.Query()["root"]
+		w.Write([]byte("<h1>" + rootStrings[0] + "</h1>"))
+
+		root, _ := hex.DecodeString(rootStrings[0])
+
+		fmt.Println(">>", root, rootStrings[0])
+		block := getBlock(ctx, ethClient, root)
+		fmt.Println(block)
+
+		// w.Write([]byte("<p>Signature: " + hex.EncodeToString(block.Signature) + "</p>"))
+		return
 	})
 
 	log.Fatal(http.ListenAndServe(":8088", nil))
